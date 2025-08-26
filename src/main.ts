7ec30331a2a58060e3c8a081187d75f53bebc75f
Kotiun <hk97903@win.edag.de>
2025-08-26 08:41:41 +0200

    succesfuly run e2e test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { resolve } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const APP_DIRECTORY = resolve(__dirname, '..');
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setViewEngine('hbs');
+  app.setBaseViewsDir(resolve(APP_DIRECTORY, 'views'));
+  app.useStaticAssets(resolve(APP_DIRECTORY, 'public'));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
